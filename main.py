315834f99ea0ffccfe874e84cd6b029e0a82a4d5
root <root@DESKTOP-UL11C45.localdomain>
2025-10-18 08:01:42 +0300

    added main project classes

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
-# 3rd party modules
 import pygame as pg
-
-# local modules
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
+from player import Player
 
 def main():
     pg.init()
     screen = pg.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+    clock = pg.time.Clock()
+    
+    updatable = pg.sprite.Group()
+    drawable = pg.sprite.Group()
+    asteroids = pg.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    Player.containers = (updatable, drawable)
+
+    asteroid_field = AsteroidField()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
     while True:
-        screen.fill((0, 0, 0))
-        pg.display.flip()
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
 
+        updatable.update(dt)
+        screen.fill("black")
+        for obj in drawable:
+            obj.draw(screen)
+        pg.display.flip()
+
+        dt = clock.tick(60) / 1000
+
 if __name__ == "__main__":
     main()
+pg.sprite
---
+++ player.py
@@ -0,0 +1,38 @@
+import pygame as pg
+from constants import *
+from circleshape import CircleShape
+
+class Player(CircleShape):
+    
+    def __init__(self, x: int, y: int):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    def triangle(self):
+        forward = pg.Vector2(0, 1).rotate(self.rotation)
+        right = pg.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen):
+        pg.draw.polygon(screen, "white", self.triangle(), 2)
+    
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+    
+    def update(self, dt):
+        keys = pg.key.get_pressed()
+        if keys[pg.K_a]:
+            self.rotate(-dt)
+        if keys[pg.K_d]:
+            self.rotate(dt)
+        if keys[pg.K_w]:
+            self.move(dt)
+        if keys[pg.K_s]:
+            self.move(-dt)
+
+    def move(self, dt):
+        forward = pg.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
